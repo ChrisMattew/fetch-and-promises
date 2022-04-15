7e061f3e308993446ca6a0884009ec6b6a122c6f
ChrisMattew <cristian.matteu@gmail.com>
2022-04-15 10:43:42 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -2,18 +2,13 @@ import { apiClient } from './api-client';
 
 let loading = false;
 
-const getSomething = async (url) => {
-  const products = await apiClient.get(`${url}`);
-  console.log(products);
-};
-
 // Resource: carts?limit=3
 const fetchCarts = () => {
   return apiClient.get('carts?limit=3');
 };
 // Resource: products/{id}
 const fetchProduct = (id: string) => {
-  if (Math.random() > 0.8) throw new Error('Network error');
+  if (Math.random() > 0) throw new Error('Network error');
   return apiClient.get(`products/${id}`);
 };
 
@@ -23,27 +18,38 @@ const fetchUser = (id: string) => {
 };
 
 const main = async () => {
+  loading = true;
   // 1. Recuperare i carrelli (carts)
   const [cart] = await fetchCarts();
   // 2. Ricostruire il carrello recuperando 'userId' e 'productId' per ogni carrello
-  const user = await fetchUser(cart.id);
-  const product = await fetchProduct(
-    cart.products.map(({ productId }) => productId)
-  );
-  console.log(product);
+  const user = await fetchUser(cart.userId);
 
-  // const newCart = Promise.all(carts.map((cart)=>));
-  // 3. Gestire correttamente il loading
+  const products = await Promise.all(
+    cart.products.map(({ productId }: any) => {
+      return fetchProduct(productId);
+    })
+  ).catch((e) => console.error(e));
+  console.log(products);
 
-  // 4. Gestire correttamente gli errori visualizzando un alert
+  // 3. Gestire correttamente il loading DONE
+
+  // 4. Gestire correttamente gli errori visualizzando un alert DONE
 
   // 5. Creare un array con top 3 prodotti più comprati
+
+  loading = false;
 };
 
 main();
+
 // fetch('https://fakestoreapi.com/products')
 //   .then((response) => {
 //     console.log(response);
 //     return response.json();
 //   })
 //   .then((products) => console.log(products));
+
+// const getSomething = async (url) => {
+//   const products = await apiClient.get(`${url}`);
+//   console.log(products);
+// };
